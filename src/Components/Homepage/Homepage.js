a7d5c2d66c79f9cae5df85651579aadbdf4ac14c
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-02-29 21:34:45 +0800

    added background and text colors

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import portFolioImage from "../../img/portfolio-img.png";
+import "../Homepage/Homepage.css";
 
 const Homepage = () => {
   const [text] = useTypewriter({
@@ -9,13 +11,28 @@ const Homepage = () => {
 
   return (
     <div>
-      <h1>
-        I am Software
-        <span> {text}</span>
-        <span>
-          <Cursor />
-        </span>
-      </h1>
+      <div className="top">
+        <div className="intro">
+          <div className="name">
+            <h1>I am John </h1>
+          </div>
+          <div>
+            <h1 className="software">
+              Software
+              <span className="engineer-developer"> {text}</span>
+              <span className="cursor">
+                <Cursor />
+              </span>
+            </h1>
+          </div>
+        </div>
+        <div className="portfolio-image">
+          <div className="image">
+            <img src={portFolioImage} alt="" />
+          </div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 };
