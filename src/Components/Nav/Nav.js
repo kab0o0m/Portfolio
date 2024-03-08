6306e25ef70764b5f4fca0879fcc1aa6c0edccf6
kab0o0m <134916365+kab0o0m@users.noreply.github.com>
2024-03-08 15:09:22 +0800

    removed bootstrap

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -3,47 +3,21 @@ import "./Nav.css";
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="container">
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div
-          className="collapse navbar-collapse justify-content-end"
-          id="navbarNav"
-        >
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item active">
-              <a className="nav-link" href="/">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Resume
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Skills
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Skills</a>
+        </li>
+        <li>
+          <a href="/">Projects</a>
+        </li>
+        <li>
+          <a href="/">Hobbies</a>
+        </li>
+      </ul>
     </nav>
   );
 };
